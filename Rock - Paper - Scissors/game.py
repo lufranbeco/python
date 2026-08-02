5b2bc8fd2fa3d130a16154c92427cfc71e02252d
Luis Becerra <lufranbeco@gmail.com>
2026-08-01 21:58:29 -0400

    feat: updating code with loop for rows

--- Rock - Paper - Scissors/game.py
+++ Rock - Paper - Scissors/game.py
@@ -1,28 +1,42 @@
 import random
 options = ['ROCK', 'PAPER', 'SCISSORS']
-user=input("\nChoose between rock, paper and scissors: ")
+while True:
+    question = int(input("\nHow many rows you wanna play?: "))
 
-computer=random.choice(options)
+    for i in range(question):
+        while True:
+            user=input("\nChoose between rock, paper and scissors: ")
+            if user.upper() != "ROCK" and user.upper() != "PAPER" and user.upper() != "SCISSORS":
+                print(f"\n{user}? that's not rock, paper or scissors, choose again")
+            else:
+                break
+            
+        computer=random.choice(options)
 
-if computer == user.upper():
-    print(f"\n(Computer) {computer} - {user.upper()} (You)")
-    print("It's a tie!")
-elif computer != user.upper():
-    if computer == 'ROCK' and user.upper() == "SCISSORS":
-        print(f"\n(Computer) {computer} -> {user.upper()} (You)")
-        print("You loose!")
-    elif computer == 'SCISSORS' and user.upper() == "ROCK":
-        print(f"\n(Computer) {computer} <- {user.upper()} (You)")
-        print("You win!")
-    elif computer == 'ROCK' and user.upper() == "PAPER":
-        print(f"\n(Computer) {computer} <- {user.upper()} (You)")
-        print("You win!")
-    elif computer == 'PAPER' and user.upper() == "ROCK":
-        print(f"\n(Computer) {computer} -> {user.upper()} (You)")
-        print("You loose!")
-    elif computer == 'PAPER' and user.upper() == "SCISSORS":
-        print(f"\n(Computer) {computer} <- {user.upper()} (You)")
-        print("You win!")
-    elif computer == 'SCISSORS' and user.upper() == "PAPER":
-        print(f"\n(Computer) {computer} -> {user.upper()} (You)")
-        print("You loose!")
+        if computer == user.upper():
+            print(f"\n(Computer) {computer} - {user.upper()} (You)")
+            print("It's a tie!")
+        elif computer != user.upper():
+            if computer == 'ROCK' and user.upper() == "SCISSORS":
+                print(f"\n(Computer) {computer} -> {user.upper()} (You)")
+                print("You loose!")
+
+            elif computer == 'SCISSORS' and user.upper() == "ROCK":
+                print(f"\n(Computer) {computer} <- {user.upper()} (You)")
+                print("You win!")
+
+            elif computer == 'ROCK' and user.upper() == "PAPER":
+                print(f"\n(Computer) {computer} <- {user.upper()} (You)")
+                print("You win!")
+
+            elif computer == 'PAPER' and user.upper() == "ROCK":
+                print(f"\n(Computer) {computer} -> {user.upper()} (You)")
+                print("You loose!")
+
+            elif computer == 'PAPER' and user.upper() == "SCISSORS":
+                print(f"\n(Computer) {computer} <- {user.upper()} (You)")
+                print("You win!")
+
+            elif computer == 'SCISSORS' and user.upper() == "PAPER":
+                print(f"\n(Computer) {computer} -> {user.upper()} (You)")
+                print("You loose!")
