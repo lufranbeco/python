98773ad4f463f6fc708b7f9bbc6e8dc482d153fe
Luis Becerra <lufranbeco@gmail.com>
2026-09-15 22:17:03 -0400

    feat: added a 'while' for reset the min-max variables if the user wanna play another round

--- Guess The Number/game.py
+++ Guess The Number/game.py
@@ -1,19 +1,28 @@
 import random 
 
-min = int(input("\nWrite the minimun number: "))
-max = int(input("\nWrite the maximun number: "))
+game = True
+while game == True:
+    min = int(input("\nWrite the minimun number: "))
+    max = int(input("\nWrite the maximun number: "))
 
-number = random.randint(min, max)
+    number = random.randint(min, max)
 
-while True:
-    user = int(input(f"\nGuess my number between {min} - {max}: "))
-    if user < min or user > max:
-        print(f"Mmm, it's between {min} and {max}")
-    elif user == number:
-        print("\n¡You guessed the number, congratulations!")
-        opcion = str(input("\n¿Do you wanna play again? y/n: "))
-        if opcion.upper() == "N":
-            print("\nIt's OK, see you soon...\n")
-            break 
-    else:
-        print("\nWrong number.. try again.")
+    while True:
+
+        user = int(input(f"\nGuess my number between {min} - {max}: "))
+
+        if user < min or user > max:
+            print(f"Mmm, it's between {min} and {max}")
+
+        elif user == number:
+            print("\n¡You guessed the number, congratulations!")
+            opcion = str(input("\n¿Do you wanna play again? y/n: "))
+
+            if opcion.upper() == "N":
+                print("\nIt's OK, see you soon...\n")
+                game=False
+                break
+            else:
+                break
+        else:
+            print("\nWrong number.. try again.")
